0d8ca6a97f5be9dd84643e9c5e1fc0e18eed9945
Rodrigo Zaninelli <rhzaninelli@gmail.com>
2022-02-25 16:28:13 -0300

    :tada: Project created

---
+++ src/main/java/com/rzaninelli/xbrain_sell_challenge/XbrainVendaDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.rzaninelli.xbrain_sell_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbrainVendaDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbrainVendaDesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rzaninelli/xbrain_sell_challenge/XbrainVendaDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rzaninelli.xbrain_sell_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainVendaDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
